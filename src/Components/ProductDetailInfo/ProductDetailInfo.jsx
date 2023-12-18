57f717177fbf8cb236b2574b4e9ac4b661a9087a
Vache Apinyan <apinyan.v@gmail.com>
2023-12-19 01:10:08 +0400

    feat(src/Components/ProductDetailInfo): change to  button styles and effects

--- src/Components/ProductDetailInfo/ProductDetailInfo.jsx
+++ src/Components/ProductDetailInfo/ProductDetailInfo.jsx
@@ -1,12 +1,15 @@
-import Button from "../Button/Button";
+import Button from "../Button/Button.jsx";
+import styles from "./ProductDetailInfo.module.css"
 
 export default function ProductDetailInfo({ product, onProductAdd }) {
   return (
     <>
       <p>
-        {product.description} sold <strong>${product.price}</strong> by value
+        {product.description} sold by <strong>${product.price}</strong> value.
       </p>
-      <Button onClick={() => onProductAdd(product)}>${product.price}</Button>
+      <Button 
+      className={styles.btn}
+      onClick={() => onProductAdd(product)}>${product.price}</Button>
     </>
   );
 }
